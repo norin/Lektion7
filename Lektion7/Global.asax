375cb0b3069b35c1376d53a9a263ba28903c8c2d
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-02 15:20:00 +0200

    Innan övningar

---
+++ Lektion7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lektion7.MvcApplication" Language="C#" %>
---
+++ Lektion7/Models/Entities/Thread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lektion7.Models.Entities.Abstract;
+
+namespace Lektion7.Models.Entities
+{
+    public class ForumThread : IEntity
+    {
+        public Guid ID { get; set; }
+        public string ThreadName { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}

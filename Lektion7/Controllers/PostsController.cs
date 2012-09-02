79cafe57989e0282039018516f3180a5c14062d0
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-02 17:42:14 +0200

    Uppgift 1

---
+++ Lektion7/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lektion7.Controllers
+{
+    public class PostsController : Controller
+    {
+        //
+        // GET: /Posts/
+
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+
+    }
+}

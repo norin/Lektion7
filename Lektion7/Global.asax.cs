3a464985f79dc90e3fe46153f4f088bf19b23beb
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-02 17:45:12 +0200

    Uppgift 2

--- Lektion7/Global.asax.cs
+++ Lektion7/Global.asax.cs
@@ -22,6 +22,12 @@ namespace Lektion7
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "API", // Route name
+                "API/{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
